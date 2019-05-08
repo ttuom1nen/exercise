9495b2db5b9e693171e7039d175453c8c130b96c
ttuom1nen <ttuominen@outlook.com>
2019-05-08 22:47:58 +0300

    Added faker for dummy data, created a comment component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  const buttonText = "Click me!";
+// Use faker for dummy data
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
+const App = () => {
   return (
-    <div>
-      <label className="label">
-        Enter name:
-        <input id="name" type="text" />
-      </label>
-      <button
-        style={{ backgroundColor: "blue", color: "white" }}
-        onClick={() => {
-          console.log(document.getElementById("name").value);
-        }}
-      >
-        {buttonText}
-      </button>
+    <div className="ui container comments">
+      <CommentDetail />
     </div>
   );
 };
