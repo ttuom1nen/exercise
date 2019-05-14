d8960129bdcb3c92275ec3dd2c85550f233e6f08
ttuom1nen <ttuominen@outlook.com>
2019-05-14 23:13:35 +0300

    Minor refactor, location now set to state

--- src/index.js
+++ src/index.js
@@ -29,13 +29,21 @@ const App = () => {
 };*/
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       err => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>;
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
