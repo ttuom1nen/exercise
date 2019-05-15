0a5a2b3c8349c561e4032ce27cb54215f132ad6e
ttuom1nen <ttuominen@outlook.com>
2019-05-15 23:09:18 +0300

    Refactor, life-cycle

--- src/index.js
+++ src/index.js
@@ -30,18 +30,12 @@ const App = () => {
 };*/
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -51,7 +45,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude:{this.state.lat}</div>;
+      return <div>Latitude: {this.state.lat}</div>;
     }
     return <div>Loading...</div>;
   }
