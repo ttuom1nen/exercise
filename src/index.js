94d349707c751df1ee30a8fd473e44bcca339884
ttuom1nen <ttuominen@outlook.com>
2019-05-17 15:42:33 +0300

    Spinner loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./components/SeasonDisplay";
+import Spinner from "./components/Spinner";
 
 // Use faker for dummy data
 import faker from "faker";
@@ -48,7 +49,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading...</div>;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
