51f380b11ae36b99c3b700a79e8e78f1fce2537c
ttuom1nen <ttuominen@outlook.com>
2019-05-19 17:43:40 +0300

    Added common container

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error:{this.state.errorMessage}</div>;
     }
@@ -50,6 +50,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
