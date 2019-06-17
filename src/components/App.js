b0f939c5a71fd4d17f8190da2d5526129e06ae95
ttuom1nen <ttuominen@outlook.com>
2019-06-17 23:50:47 +0300

    Refactoring

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./components/SeasonDisplay";
-import Spinner from "./components/Spinner";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import CommentDetail from "./components/CommentDetail";
-import ApprovalCard from "./components/ApprovalCard";
+import App from "./components/App";
+import reducers from "./reducers";
 
 /*
 const App = () => {
@@ -22,34 +22,9 @@ const App = () => {
   );
 };*/
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: "" };
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(json => console.log(json));
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error:{this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
